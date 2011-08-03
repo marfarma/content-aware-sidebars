6bb5957ae1a5603a321b69916e1c280ed6fb300e
intoxstudio <intoxstudio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-03 16:50:36 +0000

    Adding first version of my plugin
    git-svn-id: http://svn.wp-plugins.org/content-aware-sidebars/trunk@418853 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ content-aware-sidebars.php
@@ -0,0 +1,429 @@
+<?php
+/**
+ * @package Content Aware Sidebars
+ */
+/*
+Plugin Name: Content Aware Sidebars
+Plugin URI: http://www.intox.dk/
+Description: Manage and show sidebars according to the content being viewed.
+Version: 0.1
+Author: Joachim Jensen
+Author URI: http://www.intox.dk/
+License:
+
+    Copyright 2011  Joachim Jensen  (email : jv@intox.dk)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+class ContentAwareSidebars {
+	
+	public $version = 0.1;
+	public $settings = array();
+	
+	/**
+	 *
+	 * Constructor
+	 *
+	 */
+	public function __construct() {
+		
+		add_filter('wp',			array(&$this,'replace_sidebar'));
+		add_action('init',			array(&$this,'init_sidebar_type'));
+		add_action('widgets_init',		array(&$this,'create_sidebars'));
+		add_action('admin_init',		array(&$this,'create_meta_boxes'));
+		add_action('admin_head',		array(&$this,'init_settings'));
+		add_action('save_post', 		array(&$this,'save_post'));
+		
+		register_activation_hook(__FILE__,	array(&$this,'upon_activation'));
+		
+	}
+	
+	/**
+	 *
+	 * Create post meta fields
+	 * Loaded in admin_head due to $post. Should be loaded even later if possible.
+	 *
+	 */
+	public function init_settings() {
+		global $post, $wp_registered_sidebars;
+
+		// List of sidebars
+		$sidebar_list = array();
+		foreach($wp_registered_sidebars as $sidebar) {
+			if(isset($post) && $sidebar['id'] != 'ca-sidebar-'.$post->ID)
+				$sidebar_list[$sidebar['id']] = $sidebar['name'];
+		}
+		
+		// List of public post types
+		$post_type_list = array();
+		foreach(get_post_types(array('public'=>true),'objects') as $post_type)
+			$post_type_list[$post_type->name] = $post_type->label; 
+		
+		// Meta fields
+		$this->settings = array(
+			'post_types'	=> array(
+				'name'	=> 'Post Types',
+				'id'	=> 'post_types',
+				'desc'	=> '',
+				'val'	=> '',
+				'type'	=> 'select-multi',
+				'list'	=> $post_type_list
+			),
+			'handle'	=> array(
+				'name'	=> 'Handle',
+				'id'	=> 'handle',
+				'desc'	=> 'Replace host sidebar, merge with it or add sidebar manually.',
+				'val'	=> 0,
+				'type'	=> 'select',
+				'list'	=> array('Replace','Merge','Manual')
+			),
+			'host'		=> array(
+				'name'	=> 'Host Sidebar',
+				'id'	=> 'host',
+				'desc'	=> 'The sidebar that should be handled with. Nesting is possible. Manual handling makes this option superfluous.',
+				'val'	=> 'sidebar-1',
+				'type'	=> 'select',
+				'list'	=> $sidebar_list
+			),
+			'merge-pos'	=> array(
+				'name'	=> 'Merge position',
+				'id'	=> 'merge-pos',
+				'desc'	=> 'Place sidebar on top or bottom of host when merging. Merging can happen with all handlings.',
+				'val'	=> 1,
+				'type'	=> 'select',
+				'list'	=> array('Top','Bottom')
+			)
+		);
+	}
+	
+	/**
+	 *
+	 * Custom Post Type: Sidebar
+	 *
+	 */
+	public function init_sidebar_type() {
+		register_post_type('sidebar',array(
+			'labels'	=> array(
+				'name'			=> _x('Sidebars', 'post type general name'),
+				'singular_name'		=> _x('Sidebar', 'post type singular name'),
+				'add_new'		=> _x('Add New', 'sidebar'),
+				'add_new_item'		=> __('Add New Sidebar'),
+				'edit_item'		=> __('Edit Sidebar'),
+				'new_item'		=> __('New Sidebar'),
+				'all_items'		=> __('All Sidebars'),
+				'view_item'		=> __('View Sidebar'),
+				'search_items'		=> __('Search Sidebars'),
+				'not_found'		=>  __('No sidebars found'),
+				'not_found_in_trash'	=> __('No sidebars found in Trash'), 
+				'parent_item_colon'	=> '',
+				'menu_name'		=> 'Sidebars'
+			),
+			'show_ui'	=> true, 
+			'query_var'	=> false,
+			'rewrite'	=> false,
+			'menu_position' => null,
+			'supports'	=> array('title','page-attributes'),
+		));
+	}
+	
+	/**
+	 *
+	 * Create sidebars from content types
+	 *
+	 */
+	public function create_sidebars() {
+		$posts = get_posts(array( 'numberposts' => 0, 'post_type'=> 'sidebar'));
+		foreach($posts as $post)
+			register_sidebar( array(
+				'name'		=> $post->post_title,
+				'id'		=> 'ca-sidebar-'.$post->ID,
+				'before_widget'	=> '<li id="%1$s" class="widget-container %2$s">',
+				'after_widget'	=> '</li>',
+				'before_title'	=> '<h3 class="widget-title">',
+				'after_title'	=> '</h3>',
+			));
+	}
+	
+	/**
+	 *
+	 * Replace a sidebar with content aware sidebars
+	 * Handles: replace, merge.
+	 *
+	 */
+	public function replace_sidebar() {
+		global  $wp_query, $post_type, $_wp_sidebars_widgets;
+		
+		$posts = get_posts(array(
+			'numberposts'	=> 0,
+			'post_type'	=> 'sidebar',
+			'orderby'	=> 'menu_order meta_value',
+			'meta_key'	=> 'handle',
+			'order'		=> 'ASC',
+			'meta_query'	=> array(
+				array(
+					'key'		=> 'handle',
+					'value'		=> '2',
+					'compare'	=> '!='
+				)
+			)
+		));
+		$handled_already = array();
+		
+		// Variable and function are not always equal.
+		$content_type = $post_type;
+		if(!$content_type)
+			$content_type = get_post_type();
+		
+		// Only grab meta when needed.
+		foreach($posts as $post) {
+
+			$id = 'ca-sidebar-'.$post->ID;
+			$post_types = (array) unserialize(get_post_meta($post->ID, 'post_types', true));
+			
+			// Check if current post type is part of post type rules
+			if(!in_array($content_type,$post_types))
+				continue;
+		
+			$host = get_post_meta($post->ID, 'host', true);
+		
+			// Check if sidebar or host exists
+			if (!isset($_wp_sidebars_widgets[$id]))
+				continue;
+			
+			$handle = get_post_meta($post->ID, 'handle', true);	
+			
+			// If host has already been replaced, merge with it instead.
+			if($handle || isset($handled_already[$host])) {
+				$merge_pos = get_post_meta($post->ID, 'merge-pos', true);
+				if($merge_pos)
+					$_wp_sidebars_widgets[$host] = array_merge($_wp_sidebars_widgets[$host],$_wp_sidebars_widgets[$id]);
+				else
+					$_wp_sidebars_widgets[$host] = array_merge($_wp_sidebars_widgets[$id],$_wp_sidebars_widgets[$host]);
+			} else {
+				$_wp_sidebars_widgets[$host] = $_wp_sidebars_widgets[$id];
+				$handled_already[$host] = 1;
+			}		
+		}		
+	}
+	
+	/**
+	 *
+	 * Meta boxes for edit post
+	 *
+	 */
+	public function create_meta_boxes() {
+		global $post;
+		
+		add_meta_box(
+			'ca-sidebar',
+			'Options',
+			array(&$this,'meta_box_content'),
+			'sidebar',
+			'normal',
+			'high'
+		);
+		
+	}
+	
+	public function meta_box_content() {
+		
+		$this->form_fields(array('post_types','handle','merge-pos','host'));
+		
+	}
+	
+	/**
+	 *
+	 * Create form fields
+	 *
+	 */
+	private function form_fields($array) {
+		global $post;
+		
+		// Use nonce for verification
+		wp_nonce_field(basename(__FILE__),'_ca-sidebar-nonce');
+		?>
+		<table class="form-table"> 
+		<?php
+		$array = array_intersect_key($this->settings,array_flip($array));
+		foreach($array as $setting) :
+		
+		$meta = get_post_meta($post->ID, $setting['id'], true);
+		$current = $meta != '' ? $meta : $setting['val'];
+		?>
+			<tr valign="top">
+				<th scope="row"><?php echo $setting['name'] ?></th>
+				<td>
+			<?php switch($setting['type']) :
+				case 'select' :			
+					echo '<select style="width:200px;" name="'.$setting['id'].'">'."\n";
+					foreach($setting['list'] as $key => $value) {
+						echo '<option value="'.$key.'"'.($key == $current ? ' selected="selected"' : '').'>'.$value.'</option>'."\n";
+					}
+					echo '</select>'."\n";
+					break;
+				case 'select-multi' :
+					echo '<select multiple="multiple" size="5" style="width:200px;height:60px;" name="'.$setting['id'].'[]">'."\n";
+					foreach($setting['list'] as $key => $value) {
+						echo '<option value="'.$key.'"'.(in_array($key,unserialize($current)) ? ' selected="selected"' : '').'>'.$value.'</option>'."\n";
+					}
+					echo '</select>'."\n";
+					break;
+				case 'text' :
+				default :
+					echo '<input style="width:200px;" type="text" name="'.$setting['id'].'" value="'.implode(",",unserialize($current)).'" />'."\n";
+					break;
+			endswitch; ?>
+				<br /><span class="description"><?php echo $setting['desc'] ?></span>
+				</td>
+			</tr>	
+		<?php endforeach; ?>			
+		</table>
+	<?php
+	}
+	
+	/**
+	 *
+	 * Save meta values for post
+	 *
+	 */
+	public function save_post($post_id) {
+		global $post;
+		
+		
+		
+		// Save button pressed
+		if(!isset($_POST['original_publish'])) {
+			return $post_id;
+		}
+		
+		// Verify nonce
+		if (!check_admin_referer(basename(__FILE__),'_ca-sidebar-nonce')) {
+			return $post_id;
+		}
+		
+		// Check permissions
+		if (!current_user_can('edit_post', $post_id)) {
+			return $post_id;
+		}
+		
+		// Check autosave
+		if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+			return $post_id;
+		}
+		
+		// Load settings manually here. This ought to be done with action/filter
+		$this->init_settings();
+		
+		// Update values
+		foreach ($this->settings as $field) {
+			$old = get_post_meta($post_id, $field['id'], true);			
+			$new = $_POST[$field['id']];
+			
+			switch($field['id']) {	
+				case 'post_types' :
+					$new = serialize($new);
+					break;
+				default :
+					break;
+			}
+			
+			if ($new != '' && $new != $old) {
+				update_post_meta($post_id, $field['id'], $new);		
+			}elseif ($new == '' && $old != '') {
+				delete_post_meta($post_id, $field['id'], $old);	
+			}
+		}
+	}
+	
+	/**
+	 *
+	 * Flush rewrite rules on plugin activation
+	 *
+	 */
+	public function upon_activation() {
+		$this->init_sidebar_type();
+		flush_rewrite_rules();
+	}
+	
+}
+
+// Launch plugin
+global $ca_sidebars;
+$ca_sidebars = new ContentAwareSidebars();
+
+// Template function
+function display_ca_sidebar($args = array()) {
+	global $wp_query, $post_type, $_wp_sidebars_widgets;
+	
+	if(empty($args)) {
+		$args = array(
+			'before'	=> '<div id="sidebar" class="widget-area"><ul class="xoxo">',
+			'after'		=> '</ul></div>'
+		);
+	}
+	
+	$posts = get_posts(array(
+		'numberposts'	=> 0,
+		'post_type'	=> 'sidebar',
+		'orderby'	=> 'menu_order meta_value',
+		'meta_key'	=> 'handle',
+		'order'		=> 'ASC',
+		'meta_query'	=> array(
+			array(
+				'key'		=> 'handle',
+				'value'		=> '2',
+				'compare'	=> '=='
+			)
+		)
+	));
+	$content_type = $post_type;
+	if(!$content_type)
+		$content_type = get_post_type();
+		
+	$i = $host = 0;
+	foreach($posts as $post) {
+			
+		$id = 'ca-sidebar-'.$post->ID;
+		$post_types = (array) unserialize(get_post_meta($post->ID, 'post_types', true));
+		
+		// Check if current post type is part of rules
+		if(!in_array($content_type,$post_types))
+			continue;	
+		
+		if($i > 0) {
+							
+			// Check if sidebar is active
+			if (!isset($_wp_sidebars_widgets[$id]))
+				continue;
+				
+			$merge_pos = get_post_meta($post->ID, 'merge-pos', true);
+			if($merge_pos)
+				$_wp_sidebars_widgets[$host] = array_merge($_wp_sidebars_widgets[$host],$_wp_sidebars_widgets[$id]);
+			else
+				$_wp_sidebars_widgets[$host] = array_merge($_wp_sidebars_widgets[$id],$_wp_sidebars_widgets[$host]);		
+		} else {
+			$host = $id;
+		}
+		$i++;
+	}
+	
+	if ($host && is_active_sidebar($host)) {
+		echo $args['before'];
+		dynamic_sidebar($host);
+		echo $args['after'];
+	}
+	
+}
+
---
+++ readme.txt
@@ -0,0 +1,65 @@
+=== Plugin Name ===
+Contributors: intoxstudio
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=KPZHE6A72LEN4&lc=US&item_name=WordPress%20Plugin%3a%20Content%20Aware%20Sidebars&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted
+Tags: sidebar, widget, content aware, post type
+Requires at least: 3.0
+Tested up to: 3.2.1
+Stable tag: 0.1
+
+Manage and show sidebars according to the content being viewed.
+
+== Description ==
+
+Manage an infinite number of sidebars. Each with different rules for which content they should be visible with. Creating flexible sidebars has never been easier, and no code is needed at all as everything is done in a simple GUI.
+
+Current features include:
+
+* Show sidebars with specific post types only
+* Merge new sidebars with others, replace them or simply add them to your theme manually
+* Create complex content with nested sidebars
+* Standard Custom Post Type features (status, visibility, publish date)
+
+Upcoming features:
+
+* Show sidebars with specific taxonomies
+* Show sidebars in a time span only
+
+If you have any suggestions, please send me a mail at jv@intox.dk.
+
+== Installation ==
+
+Do as follows
+
+1. Upload the full plugin directory to your '/wp-content/plugins/' directory or install the plugin through 'Plugins' in the administration 
+1. Activate the plugin through 'Plugins' in the administration
+1. Have fun creating your first sidebar
+1. Optional: Insert '<?php display_ca_sidebar(); ?>' in a template if you have a special spot for the new, manual handled, sidebars.
+
+== Frequently Asked Questions ==
+
+= Who's great? =
+
+You are.
+
+== Screenshots ==
+
+1. Add a new sidebar 'For Pages' visible on all pages, replacing 'Main Sidebar'
+2. Add widgets to 'For Pages'
+3. Viewing home page. 'Main Sidebar' is visible
+4. Viewing a page. 'For Pages' has replaced 'Main Sidebar'
+
+== Changelog ==
+
+= 0.1 =
+
+* First stable release
+
+== Upgrade Notice ==
+
+= 0.1 =
+
+* Hello World
+
+== Translations ==
+
+None yet. Might come in the future. Do you want to contribute? Feel free to contact me at jv@intox.dk
